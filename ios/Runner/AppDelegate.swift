100782dd554e14026295bbc4b70ea251895cc9e3
fteodoro803 <agustinteodoro803@gmail.com>
2025-02-19 19:26:54 +1100

    ios setup

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,18 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    // Google Maps API key
+    GMSServices.provideAPIKey("API_KEY_HERE")
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
