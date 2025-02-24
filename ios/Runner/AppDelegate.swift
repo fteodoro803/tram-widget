9dff0b685a93dbf095020177f51943dc525ce2ea
fteodoro803 <agustinteodoro803@gmail.com>
2025-02-24 16:55:07 +1100

    added google maps support, and set minimum ios version 16.6

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,18 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+//  Google Maps
+    GMSServices.provideAPIKey("YOUR_API_KEY_HERE")
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
